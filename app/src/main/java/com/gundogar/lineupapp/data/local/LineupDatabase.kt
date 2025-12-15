fdd3fe9b858d3ef7a55eda34c6b4e81a277e339f
Muhammet Gündoğar <muhammetemingundogar53@gmail.com>
2025-12-15 20:43:15 +0300

    Add LineupDatabase file

---
+++ app/src/main/java/com/gundogar/lineupapp/data/local/LineupDatabase.kt
@@ -0,0 +1,35 @@
+package com.gundogar.lineupapp.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.gundogar.lineupapp.data.local.dao.SavedLineupDao
+import com.gundogar.lineupapp.data.local.entity.SavedLineupEntity
+
+@Database(
+    entities = [SavedLineupEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class LineupDatabase : RoomDatabase() {
+
+    abstract fun savedLineupDao(): SavedLineupDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: LineupDatabase? = null
+
+        fun getDatabase(context: Context): LineupDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    LineupDatabase::class.java,
+                    "lineup_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
